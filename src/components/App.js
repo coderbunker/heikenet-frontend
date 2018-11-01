41f4e2005dddd148d6347527676679542b92c1f6
Joy <joy.js.mun@gmail.com>
2018-11-01 13:24:28 +0800

    remove chooseaccount.js  chooseaccount.scss

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,6 @@ class App extends Component {
             <Route exact path="/about" component={About}/>
             <Route exact path="/signIn" component={SignIn}/>
             <Route exact path="/confirmpayment" component={ConfirmPayment}/>
-            <Route exact path="/chooseaccount" component={ChooseAccount}/>
             <Route exact path="/signup_freelancer" component={SignUpFormFreelancer}/>
             <Route exact path="/signup_project" component={SignUpFormProject}/>
             <Route exact path="/signup_form" component={SignUpForm}/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export {default as SignIn} from './SignIn';
 export {default as PageNotFound} from './PageNotFound';
 export {default as Navbar} from './CustomNavBar';
 export {default as ConfirmPayment} from './ConfirmPayment';
-export {default as ChooseAccount} from './ChooseAccount';
+
 export {default as SignUpFormFreelancer} from './SignUpFormFreelancer';
 export {default as SignUpFormProject} from './SignUpFormProject';
 export {default as UserInfo} from './UserInfo';
